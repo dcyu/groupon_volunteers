8152ee8b6f8628c2f68e5abe2de155c477982528
Marshall Shen <mshen@groupon.com>
2013-06-10 22:50:13 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrouponVolunteers::Application.config.secret_key_base = 'd57929618d7c0cf49e6caee4ac884c7dc839acebaa6b526d8072b2f5bfd4edf4ae16de77932697b55fb7a0c245ba2764d3d35a5416d2661e9a9bcebd3344b42c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrouponVolunteers::Application.config.session_store :cookie_store, key: '_groupon_volunteers_session'
